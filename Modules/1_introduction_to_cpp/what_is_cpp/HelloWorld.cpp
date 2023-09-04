61eaaf5b2346fea9a1c5ba8007ae946c64c66075
Temps <temps0602@gmail.com>
2023-09-03 19:13:48 -0500

    Create HelloWorld.cpp

---
+++ Modules/1_introduction_to_cpp/what_is_cpp/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// This line includes the standard input-output stream library.
+// It allows us to use functions and objects for input-output operations.
+#include<iostream>
+
+// The main function is where the program starts executing.
+int main()
+{
+    // The 'cout' object is used to display output to the console.
+    // Here, we output the string "Hello, World!".
+    std::cout << "Hello, World!" << std::endl;
+
+    // This line signifies the successful termination of the program.
+    // It returns an exit status of 0.
+    return 0;
+}
